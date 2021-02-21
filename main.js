420cd73ab910b04dab17503d3f83321d2e1af468
farizash <rashidovna2196@gmail.com>
2021-02-22 02:27:37 +0600

    js homewor

---
+++ main.js
@@ -0,0 +1,63 @@
+//1 задача
+var str = 'Привет, Мир!'
+document.write('<h4>' + str + '</h4>')
+console.log(str)
+
+//alert('Привет, Мир!')
+
+
+
+//2 задача
+var str1 = 'Привет, '
+var str2 = 'Мир!'
+document.write(`<hr><p> ${str1 + str2} </p>`)
+
+
+//3 задача
+// 1 сагат = 60 мин
+// 1 мин = 60 сек  (60 * 60)
+const time1 = 60
+const time2 = 60
+document.write(`<hr><p>Количество секунд в часе: ${time1 * time2}</p>`)
+
+
+//4 задача
+var a = 10
+var b = 2
+document.write(`<hr><p>Сумма: 10 + 2 = ${a + b}</p>`)
+document.write(`<p>Разность: 10 - 2 = ${a - b}</p>`)
+document.write(`<p>Произведение: 10 * 2 = ${a * b}</p>`)
+document.write(`<p>Результат деления: 10 / 2 = ${a / b}</p>`)
+
+//5 задача
+var c = 15
+var d = 2
+var result = c + d
+document.write(`<hr><p>Сумма: 15 + 2 = ${result}</p>`)
+
+//6 задача
+var a = 10
+var b = 2
+var c = 5
+document.write(`<hr><p>Сумма: 10 + 2 + 5 = ${a + b + c}</p>`)
+
+
+//7 задача
+var a = 4
+var b = 2
+var g = Math.sqrt((a * a) + (b * b));
+document.write(`<hr><p>Гипотенуза: ${g}</p>`)
+
+
+//8 задача
+var a = 5
+var b = 9
+var c = 16
+var allIn = (a + b + c) / 3
+document.write(`<hr><p>Среднее значение: ${allIn}</p>`) 
+
+
+
+
+
+
